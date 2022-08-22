91efa4e6855e2cec2481ba86852aadf28ac220db
Ali Stoica Kovacs <alistoicakovacs@icloud.com>
2022-08-22 20:26:59 +0200

    practice code lines

--- script.js
+++ script.js
@@ -6,9 +6,10 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--sow-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -17,6 +18,8 @@ const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
@@ -29,3 +32,28 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+////////////////////
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent =
+  'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class ="btn btn--close-cookie">Got it!</button>';
+header.append(message);
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    //new way
+    // message.remove();
+    //old way
+    message.parentElement.removeChild(message);
+  });
