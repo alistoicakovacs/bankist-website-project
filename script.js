bc1916e84b2a7d7eb9755e25352725382aae87e6
Ali Stoica Kovacs <alistoicakovacs@icloud.com>
2022-08-27 17:43:30 +0200

    Finished the component slider

--- script.js
+++ script.js
@@ -457,94 +457,95 @@ imgTargets.forEach(img => imgObserver.observe(img));
 
 //////////////////////////////////////////
 // Building a Slider Component - Part 1
+const slider = function () {
+  const slides = document.querySelectorAll('.slide');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const dotCotainer = document.querySelector('.dots');
 
-const slides = document.querySelectorAll('.slide');
-const slider = document.querySelector('.slider');
-const btnLeft = document.querySelector('.slider__btn--left');
-const btnRight = document.querySelector('.slider__btn--right');
-const dotCotainer = document.querySelector('.dots');
+  let currSlide = 0;
+  const maxSlide = slides.length;
 
-let currSlide = 0;
-const maxSlide = slides.length;
-
-// FUNCTIONS
-// Creating the dots
-const createDots = function () {
-  // the " _ " is a conventional of the throwaway variable
-  slides.forEach(function (_, i) {
-    dotCotainer.insertAdjacentHTML(
-      'beforeend',
-      `
+  // FUNCTIONS
+  // Creating the dots
+  const createDots = function () {
+    // the " _ " is a conventional of the throwaway variable
+    slides.forEach(function (_, i) {
+      dotCotainer.insertAdjacentHTML(
+        'beforeend',
+        `
       <button class="dots__dot" data-slide="${i}"></button>
     `
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add('dots__dot--active');
+  };
+
+  // Go to slide function
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
     );
+  };
+
+  // Next slide
+  const nextSlide = function () {
+    if (currSlide === maxSlide - 1) {
+      currSlide = 0;
+    } else {
+      currSlide++;
+    }
+
+    goToSlide(currSlide);
+    activateDot(currSlide);
+  };
+
+  // Previous Slide
+  const prevSlide = function () {
+    if (currSlide === 0) {
+      currSlide = maxSlide - 1;
+    } else {
+      currSlide--;
+    }
+    goToSlide(currSlide);
+    activateDot(currSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+    activateDot(0);
+  };
+  init();
+
+  // EVENT HANDLERS
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+
+  // KEYBOARD EVENTS
+  document.addEventListener('keydown', function (e) {
+    // console.log(e);
+    if (e.key === 'ArrowLeft') prevSlide();
+    // Short cirquiting
+    e.key === 'ArrowRight' && nextSlide();
+  });
+
+  // Event listener for the dots
+  dotCotainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
   });
 };
-
-const activateDot = function (slide) {
-  document
-    .querySelectorAll('.dots__dot')
-    .forEach(dot => dot.classList.remove('dots__dot--active'));
-
-  document
-    .querySelector(`.dots__dot[data-slide="${slide}"]`)
-    .classList.add('dots__dot--active');
-};
-
-// Go to slide function
-const goToSlide = function (slide) {
-  slides.forEach(
-    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-  );
-};
-
-// Next slide
-const nextSlide = function () {
-  if (currSlide === maxSlide - 1) {
-    currSlide = 0;
-  } else {
-    currSlide++;
-  }
-
-  goToSlide(currSlide);
-  activateDot(currSlide);
-};
-
-// Previous Slide
-const prevSlide = function () {
-  if (currSlide === 0) {
-    currSlide = maxSlide - 1;
-  } else {
-    currSlide--;
-  }
-  goToSlide(currSlide);
-  activateDot(currSlide);
-};
-
-const init = function () {
-  goToSlide(0);
-  createDots();
-  activateDot(0);
-};
-init();
-
-// EVENT HANDLERS
-btnRight.addEventListener('click', nextSlide);
-btnLeft.addEventListener('click', prevSlide);
-
-// KEYBOARD EVENTS
-document.addEventListener('keydown', function (e) {
-  // console.log(e);
-  if (e.key === 'ArrowLeft') prevSlide();
-  // Short cirquiting
-  e.key === 'ArrowRight' && nextSlide();
-});
-
-// Event listener for the dots
-dotCotainer.addEventListener('click', function (e) {
-  if (e.target.classList.contains('dots__dot')) {
-    const { slide } = e.target.dataset;
-    goToSlide(slide);
-    activateDot(slide);
-  }
-});
+slider();
